034bb29c8867fea8fa5cd5412fbf224e26e09447
Joseth <jojo.programmer01@gmail.com>
2025-10-30 21:31:46 +0000

    fix: reorder highlight.js imports in entry-client.js

--- page/src/entry-client.js
+++ page/src/entry-client.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
-import 'highlight.js/lib/common'
 import 'highlight.js/styles/tokyo-night-dark.css'
+import 'highlight.js/lib/common'
 import hljsVuePlugin from '@highlightjs/vue-plugin'
 
 import { createApp } from './main'
