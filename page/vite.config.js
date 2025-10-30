6b2ee4758998c0aa35683ddc73768a638d0b42c6
Joseth <jojo.programmer01@gmail.com>
2025-10-30 21:25:23 +0000

    refactor: remove rollup-plugin-visualizer and update package versions; re-enable highlight.js styles

--- page/vite.config.js
+++ page/vite.config.js
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue({ ...templateCompilerOptions }), tailwindcss(), visualizer({ open: true })],
+  plugins: [vue({ ...templateCompilerOptions }), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
